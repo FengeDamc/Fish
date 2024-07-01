69248e1d40df0fed4045a5ffbfecea0afe78e645
MSI <bbt5xh@Hotmail.com>
2024-07-01 11:47:02 +0800

    add Autocliecker-DoubleMode:在鼠标点击时自动模拟双倍点击

--- src/com/fun/client/mods/combat/AutoClicker.java
+++ src/com/fun/client/mods/combat/AutoClicker.java
@@ -1,71 +1,140 @@
 package com.fun.client.mods.combat;
 
 import com.fun.eventapi.event.events.EventTick;
-import com.fun.utils.version.fields.Fields;
-import com.fun.utils.version.methods.Methods;
 import com.fun.client.mods.Category;
 import com.fun.client.mods.Module;
 import com.fun.client.settings.Setting;
 import com.fun.inject.inject.wrapper.impl.setting.KeyBindingWrapper;
+import com.fun.utils.version.fields.Fields;
+import com.fun.utils.version.methods.Methods;
+
+import java.awt.AWTException;
+import java.awt.Robot;
+import java.awt.event.InputEvent;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class AutoClicker extends Module {
-    //#define MOUSEEVENTF_LEFTDOWN 0x0002
-    //#define MOUSEEVENTF_LEFTUP 0x0004
-    //#define MOUSEEVENTF_RIGHTDOWN 0x0008
-    //#define MOUSEEVENTF_RIGHTUP 0x0010
-    public static final int MOUSEEVENTF_LEFTDOWN = 0x0002;
-    public static final int MOUSEEVENTF_LEFTUP = 0x0004;
-    public static final int MOUSEEVENTF_RIGHTDOWN = 0x0008;
-    public static final int MOUSEEVENTF_RIGHTUP = 0x0010;
 
+    public Setting LeftCPS = new Setting("LeftCPS", this, 12, 0, 20, true);
+    public Setting RightCPS = new Setting("RightCPS", this, 12, 0, 20, true);
+    public Setting left = new Setting("LeftClick", this, true);
+    public Setting right = new Setting("RightClick", this, true);
+    public Setting doubleMode = new Setting("Double Mode", this, false);
 
-
-
-
-    public Setting lcps =new Setting("LCPS",this,12,0,20,true);
-    public Setting rcps=new Setting("RCPS",this,12,0,20,true);
-
-    public Setting left=new Setting("Left",this,true);
-    public Setting right=new Setting("Right",this,true);
-
+    private Robot robot;
+    private volatile boolean isLeftClicking = false;
+    private volatile boolean isRightClicking = false;
+    private Timer leftClickTimer;
+    private Timer rightClickTimer;
+    private static final Logger LOGGER = Logger.getLogger(AutoClicker.class.getName());
 
     public AutoClicker() {
-        super("AutoClicker",Category.Combat);
+        super("AutoClicker", Category.Combat);
+        try {
+            robot = new Robot();
+        } catch (AWTException e) {
+            LOGGER.log(Level.SEVERE, "Failed to initialize Robot", e);
+        }
     }
 
     @Override
     public void onTick(EventTick event) {
         super.onTick(event);
 
-        if(Math.random()< lcps.getValDouble()/20){
-            if(mc.getGameSettings().getKey("key.attack").isPressed()&&left.getValBoolean()){
-                //NativeUtils.clickMouse(MOUSEEVENTF_LEFTDOWN);
-                //NativeUtils.clickMouse(MOUSEEVENTF_LEFTUP);
-                sendClick(mc.getGameSettings().getKey("key.attack"),true);
-                //sendClick(0,false);
-
-
-            }
-
-
-
+        if (doubleMode.getValBoolean()) {
+            // New logic for double mode
+            handleNewLogic();
+        } else {
+            // Old logic
+            handleOldLogic();
         }
-        //System.out.println("aimbot2");
-        if(Math.random()< rcps.getValDouble()/20)
-            if(mc.getGameSettings().getKey("key.use").isPressed()&&right.getValBoolean())
-                if(!mc.getPlayer().isUsingItem()){
-                    sendClick(mc.getGameSettings().getKey("key.use"),true);
-                    //sendClick(1,false);//tClickMouse();//MD: wn/bS ()Z net/minecraft/entity/player/EntityPlayer/func_71039_bw ()Z
-                }
+    }
 
+    private void handleNewLogic() {
+        if (mc.getGameSettings().getKey("key.attack").isPressed() && left.getValBoolean()) {
+            if (!isLeftClicking) {
+                isLeftClicking = true;
+                leftClickTimer = startClickingTimer(LeftCPS.getValDouble(), InputEvent.BUTTON1_DOWN_MASK);
+            }
+        } else {
+            isLeftClicking = false;
+            if (leftClickTimer != null) {
+                leftClickTimer.cancel();
+            }
+        }
+
+        if (mc.getGameSettings().getKey("key.use").isPressed() && right.getValBoolean()) {
+            if (!isRightClicking) {
+                isRightClicking = true;
+                rightClickTimer = startClickingTimer(RightCPS.getValDouble(), InputEvent.BUTTON3_DOWN_MASK);
+            }
+        } else {
+            isRightClicking = false;
+            if (rightClickTimer != null) {
+                rightClickTimer.cancel();
+            }
+        }
+    }
+
+    private void handleOldLogic() {
+        if (Math.random() < LeftCPS.getValDouble() / 20) {
+            if (mc.getGameSettings().getKey("key.attack").isPressed() && left.getValBoolean()) {
+                sendClick(mc.getGameSettings().getKey("key.attack"), true);
+            }
+        }
+
+        if (Math.random() < RightCPS.getValDouble() / 20) {
+            if (mc.getGameSettings().getKey("key.use").isPressed() && right.getValBoolean()) {
+                if (!mc.getPlayer().isUsingItem()) {
+                    sendClick(mc.getGameSettings().getKey("key.use"), true);
+                }
+            }
+        }
+    }
+
+    private Timer startClickingTimer(double cps, int mouseButton) {
+        Timer clickTimer = new Timer(true);
+        clickTimer.scheduleAtFixedRate(new TimerTask() {
+            @Override
+            public void run() {
+                sendClick(mouseButton);
+            }
+        }, 0, (long) (1000 / cps));
+        return clickTimer;
+    }
+
+    public void sendClick(int mouseButton) {
+        robot.mousePress(mouseButton);
+        robot.mouseRelease(mouseButton);
     }
 
     public void sendClick(final KeyBindingWrapper button, final boolean state) {
-        final int keyBind = button.getKeyCode();//KeyBinding.setKeyBindState(keyBind, state); func_74510_a
-        Fields.pressTime_KeyBinding.set(button.keyBindingObj,0);
-        Methods.setKeyBindState_KeyBinding.invoke(null,keyBind,state);
+        final int keyBind = button.getKeyCode();
+        Fields.pressTime_KeyBinding.set(button.keyBindingObj, 0);
+        Methods.setKeyBindState_KeyBinding.invoke(null, keyBind, state);
         if (state) {
-            Methods.onTick_KeyBinding.invoke(null,keyBind);//KeyBinding.onTick(keyBind);
+            Methods.onTick_KeyBinding.invoke(null, keyBind);
+        }
+    }
+
+    @Override
+    public void onEnable() {
+        super.onEnable();
+    }
+
+    @Override
+    public void onDisable() {
+        super.onDisable();
+        isLeftClicking = false;
+        isRightClicking = false;
+        if (leftClickTimer != null) {
+            leftClickTimer.cancel();
+        }
+        if (rightClickTimer != null) {
+            rightClickTimer.cancel();
         }
     }
 }
