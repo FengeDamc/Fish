069f62539786cd054714ee91e9644977d9822269
FengeDamc <fengedamc@outlook.com>
2024-07-20 12:22:21 +0800

    修复了1.18.1原版bind失效的问题

--- Injections/1.18.1/Injection1_18_1/src/com/fun/inject/injection/asm/transformers/KeyBindTransformer.java
+++ Injections/1.18.1/Injection1_18_1/src/com/fun/inject/injection/asm/transformers/KeyBindTransformer.java
@@ -2,8 +2,12 @@ package com.fun.inject.injection.asm.transformers;
 
 import com.fun.eventapi.EventManager;
 import com.fun.eventapi.event.events.EventKey;
+import com.fun.inject.Mappings;
 import com.fun.inject.injection.asm.api.Inject;
 import com.fun.inject.injection.asm.api.Transformer;
+import com.fun.inject.mapper.Mapper;
+
+import com.mojang.blaze3d.platform.InputConstants;
 import org.objectweb.asm.Opcodes;
 import org.objectweb.asm.Type;
 import org.objectweb.asm.tree.InsnList;
@@ -13,14 +17,17 @@ import org.objectweb.asm.tree.VarInsnNode;
 
 public class KeyBindTransformer extends Transformer {
     public KeyBindTransformer() {
-        super("net/minecraft/client/settings/KeyBinding");
+        super("net/minecraft/client/KeyMapping");
     }
-    @Inject(method = "onTick",descriptor = "(I)V")
+    @Inject(method = "click",descriptor = "(Lcom/mojang/blaze3d/platform/InputConstants$Key;)V")
     public void onKey(MethodNode methodNode) {
         InsnList list = new InsnList();
-        list.add(new VarInsnNode(Opcodes.ILOAD,0));
-        list.add(new MethodInsnNode(Opcodes.INVOKESTATIC, Type.getInternalName(KeyBindTransformer.class),"onKey","(I)V"));
+        list.add(new VarInsnNode(Opcodes.ALOAD,0));
+        list.add(new MethodInsnNode(Opcodes.INVOKEVIRTUAL,Mappings.getObfClass("com/mojang/blaze3d/platform/InputConstants$Key"), Mappings.getObfMethod("m_84873_"),"()I"));
+        //MD: com/mojang/blaze3d/platform/InputConstants$Key/m_84873_ ()I com/mojang/blaze3d/platform/InputConstants$Key/getValue ()I
+        list.add(new MethodInsnNode(Opcodes.INVOKESTATIC, Type.getInternalName(KeyBindTransformer.class),"onKey", Mapper.getObfMethodDesc("(I)V")));
         methodNode.instructions.insert(list);
+
 
 
     }
