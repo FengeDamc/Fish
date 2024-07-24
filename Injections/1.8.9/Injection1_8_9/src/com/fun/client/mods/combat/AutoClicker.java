c4a7707a0836ad557bf4cabd671f3235bbf377d7
MSI <bbt5xh@Hotmail.com>
2024-07-24 20:46:52 +0800

    AimAssist - Require_click,MinFov add，AutoClicker - remove double_mode，reach optimization，Target - MinRange add

--- Injections/1.8.9/Injection1_8_9/src/com/fun/client/mods/combat/AutoClicker.java
+++ Injections/1.8.9/Injection1_8_9/src/com/fun/client/mods/combat/AutoClicker.java
@@ -9,9 +9,6 @@ import com.fun.utils.version.fields.Fields;
 import com.fun.utils.version.methods.Methods;
 
 import java.awt.*;
-import java.awt.event.InputEvent;
-import java.util.Timer;
-import java.util.TimerTask;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -21,19 +18,13 @@ public class AutoClicker extends Module {
     public Setting RightCPS = new Setting("RightCPS", this, 12, 0, 20, true);
     public Setting left = new Setting("LeftClick", this, true);
     public Setting right = new Setting("RightClick", this, true);
-    public Setting doubleMode = new Setting("Double Mode", this, false);
 
-    private Robot robot;
-    private volatile boolean isLeftClicking = false;
-    private volatile boolean isRightClicking = false;
-    private Timer leftClickTimer;
-    private Timer rightClickTimer;
     private static final Logger LOGGER = Logger.getLogger(AutoClicker.class.getName());
 
     public AutoClicker() {
         super("AutoClicker", Category.Combat);
         try {
-            robot = new Robot();
+            new Robot();
         } catch (AWTException e) {
             LOGGER.log(Level.SEVERE, "Failed to initialize Robot", e);
         }
@@ -42,49 +33,18 @@ public class AutoClicker extends Module {
     @Override
     public void onTick(EventTick event) {
         super.onTick(event);
-
-        if (doubleMode.getValBoolean()) {
-            // New logic for double mode
-            handleNewLogic();
-        } else {
-            // Old logic
-            handleOldLogic();
-        }
+        handleClickLogic(); // 处理点击逻辑
     }
 
-    private void handleNewLogic() {
-        if (mc.getGameSettings().getKey("key.attack").isPressed() && left.getValBoolean()) {
-            if (!isLeftClicking) {
-                isLeftClicking = true;
-                leftClickTimer = startClickingTimer(LeftCPS.getValDouble(), InputEvent.BUTTON1_DOWN_MASK);
-            }
-        } else {
-            isLeftClicking = false;
-            if (leftClickTimer != null) {
-                leftClickTimer.cancel();
-            }
-        }
-
-        if (mc.getGameSettings().getKey("key.use").isPressed() && right.getValBoolean()) {
-            if (!isRightClicking) {
-                isRightClicking = true;
-                rightClickTimer = startClickingTimer(RightCPS.getValDouble(), InputEvent.BUTTON3_DOWN_MASK);
-            }
-        } else {
-            isRightClicking = false;
-            if (rightClickTimer != null) {
-                rightClickTimer.cancel();
-            }
-        }
-    }
-
-    private void handleOldLogic() {
+    private void handleClickLogic() {
+        // 左键点击
         if (Math.random() < LeftCPS.getValDouble() / 20) {
             if (mc.getGameSettings().getKey("key.attack").isPressed() && left.getValBoolean()) {
                 sendClick(mc.getGameSettings().getKey("key.attack"), true);
             }
         }
 
+        // 右键点击
         if (Math.random() < RightCPS.getValDouble() / 20) {
             if (mc.getGameSettings().getKey("key.use").isPressed() && right.getValBoolean()) {
                 if (!mc.getPlayer().isUsingItem()) {
@@ -92,22 +52,6 @@ public class AutoClicker extends Module {
                 }
             }
         }
-    }
-
-    private Timer startClickingTimer(double cps, int mouseButton) {
-        Timer clickTimer = new Timer(true);
-        clickTimer.scheduleAtFixedRate(new TimerTask() {
-            @Override
-            public void run() {
-                sendClick(mouseButton);
-            }
-        }, 0, (long) (1000 / cps));
-        return clickTimer;
-    }
-
-    public void sendClick(int mouseButton) {
-        robot.mousePress(mouseButton);
-        robot.mouseRelease(mouseButton);
     }
 
     public void sendClick(final KeyBindingWrapper button, final boolean state) {
@@ -127,13 +71,5 @@ public class AutoClicker extends Module {
     @Override
     public void onDisable() {
         super.onDisable();
-        isLeftClicking = false;
-        isRightClicking = false;
-        if (leftClickTimer != null) {
-            leftClickTimer.cancel();
-        }
-        if (rightClickTimer != null) {
-            rightClickTimer.cancel();
-        }
     }
 }
